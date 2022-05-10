13741d6ff07b92ab72513ab88b13133a2bd880b5
jihoon <dhrj456@naver.com>
2022-05-10 17:49:34 +0900

    20220510

---
+++ include.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.rcParams['font.family'] ='Malgun Gothic'
+matplotlib.rcParams['axes.unicode_minus'] =False
+
+
+# In[ ]:
+
+
+
+
